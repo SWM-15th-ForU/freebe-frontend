b22e8b772700ed8dae5f9fdc7f102f1e1d6b0e0d
eujin-shin <eujinshin0124@gmail.com>
2024-07-12 15:10:08 +0900

    FU-53 feat: add kakao provider by next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+
+const handler = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {},
+  pages: {
+    // signIn: "/",
+  },
+});
+
+export { handler as GET, handler as POST };
