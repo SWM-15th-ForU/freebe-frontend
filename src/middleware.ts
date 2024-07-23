bcd1dad3343877e3a4d40ec763e23534c9498711
eujin-shin <eujinshin0124@gmail.com>
2024-07-23 17:28:08 +0900

    FU-84 style: remove unnecessary comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,15 +14,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/",
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    // "/((?!api|_next/static|_next/image|.*\\.svg$).*)",
-  ],
+  matcher: ["/"],
 };
