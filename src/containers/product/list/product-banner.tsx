5d74ce6b9b42d67f46b29e1a08d6236c6aac5698
eujin-shin <eujinshin0124@gmail.com>
2024-08-01 10:35:26 +0900

    FU-81 style: add TODO comment

--- src/containers/product/list/product-banner.tsx
+++ src/containers/product/list/product-banner.tsx
@@ -27,6 +27,7 @@ const ProductBanner = ({
       <Image src={image} alt="상품 메인 이미지" style={{ width: 70 }} />
       <div style={{ flex: 1 }}>
         <Body15SB>{title}</Body15SB>
+        {/* [TODO] 예약 건수를 구하는 범위에 따라 문구 수정 필요 */}
         <Body15SB>예약 건수 {totalReservation} 건</Body15SB>
         <Switch checked={isOpen} onChange={handleSwitchOpen} />
       </div>
