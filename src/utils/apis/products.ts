0d0a7db130a5ad912b368beb2ab7314da56457d6
eujin-shin <eujinshin0124@gmail.com>
2024-08-13 17:00:03 +0900

    FU-75 fix: change api request path and body

--- src/utils/apis/products.ts
+++ src/utils/apis/products.ts
@@ -3,7 +3,6 @@ import { Image, ProductFormdata } from "product-types";
 export async function postProduct(data: ProductFormdata, images: Image[]) {
   // TODO: use core/api
   const body = {
-    memberId: 2,
     productTitle: data.title,
     productDescription: data.subtitle,
     productImageUrls: images,
@@ -30,15 +29,17 @@ export async function postProduct(data: ProductFormdata, images: Image[]) {
       };
     }),
   };
-  console.log(body);
   try {
-    const response = await fetch("https://api.freebe.co.kr/product/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+    const response = await fetch(
+      "https://api.freebe.co.kr/photographer/product/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
       },
-      body: JSON.stringify(body),
-    });
+    );
     const result = await response.json();
     console.log("success: ", result);
   } catch (error) {
