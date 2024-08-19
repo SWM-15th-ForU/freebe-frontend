7aa0ce7f80f7c803b92c8eccbc86a2e310be10c0
eujin-shin <eujinshin0124@gmail.com>
2024-08-19 11:33:21 +0900

    FU-152 feat: connect photographer url to UserData

--- src/containers/main/header.tsx
+++ src/containers/main/header.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import useUserInfo from "@/hooks/useUserInfo";
 import * as styles from "./header/header.css";
 import Profile from "./header/profile";
 import Url from "./header/url";
 
 const Header = () => {
-  // [TODO] 회원가입 후 api 요청과 연결
-  const myUrl = "https://www.freebe.co.kr/myid";
+  const { userData, userRole } = useUserInfo();
 
   return (
     <header className={styles.headerContainer}>
@@ -13,7 +15,7 @@ const Header = () => {
         <div>메인</div>
       </Link>
       <Profile name="ForU" />
-      <Url myUrl={myUrl} />
+      <Url myUrl={userRole === "photographer" ? userData.profileUrl : ""} />
     </header>
   );
 };
