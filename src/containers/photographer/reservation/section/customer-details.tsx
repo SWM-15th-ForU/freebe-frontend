7da18abe72d5524ff6bcaa9f087d7cb52e142059
eujin-shin <eujinshin0124@gmail.com>
2024-09-06 11:12:04 +0900

    FU-114 refactor: set common section layout

--- src/containers/photographer/reservation/section/customer-details.tsx
+++ src/containers/photographer/reservation/section/customer-details.tsx
@@ -1,16 +1,16 @@
+import SectionLayout from "./section-layout";
 import Field from "./field";
 import { sectionStyles } from "./section.css";
 
 const CustomerDetails = () => {
   return (
-    <div className={sectionStyles.container}>
-      <span className={sectionStyles.title}>신청자 정보</span>
+    <SectionLayout title="신청자 정보">
       <div className={sectionStyles.wrapper}>
         <Field name="이름" formField="customer.name" />
         <Field name="전화번호" formField="customer.phoneNumber" />
         <Field name="인스타그램 아이디" formField="customer.instagramId" />
       </div>
-    </div>
+    </SectionLayout>
   );
 };
 
