7cf906bc0073bd2f163be3337107c022221e9d87
eujin-shin <eujinshin0124@gmail.com>
2024-09-12 18:34:26 +0900

    FU-185 feat: get current profile of photographer

--- src/types/profile-types.d.ts
+++ src/types/profile-types.d.ts
@@ -1,6 +1,21 @@
 declare module "profile-types" {
-  export interface LinkType {
+  interface LinkType {
     src: string;
     name: string;
+  }
+
+  interface ProfileResponse {
+    bannerImageUrl: string;
+    profileImageUrl: string;
+    instagramId: string;
+    introductionContent: string;
+    linkInfos: { linkTitle: string; linkUrl: string }[];
+  }
+  interface Photographer {
+    banner: string;
+    profileImg: string;
+    instagramId: string;
+    message: string;
+    linkInfos: LinkType[];
   }
 }
