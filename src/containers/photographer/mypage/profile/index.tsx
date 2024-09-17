0cfd7833c65545032c474a16d2b66afccbda1ad5
eujin-shin <eujinshin0124@gmail.com>
2024-09-17 13:57:17 +0900

    FU-185 feat: put changed profile

--- src/containers/photographer/mypage/profile/index.tsx
+++ src/containers/photographer/mypage/profile/index.tsx
@@ -1,19 +1,36 @@
 "use client";
 
 import { Photographer } from "profile-types";
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { putProfile } from "@/services/client/photographer/mypage/profile";
+import popToast from "@/components/common/toast";
 import ProfileEdit from "./edit";
 import { profileStyles } from "./profile.css";
 import Preview from "./preview";
+import SubmitProfile from "./submit";
 
 const MyProfile = ({ profile }: { profile: Photographer }) => {
   const method = useForm<Photographer>({ defaultValues: profile });
+  const { handleSubmit } = method;
+
+  const onSubmit: SubmitHandler<Photographer> = async (data) => {
+    try {
+      await putProfile(data);
+      popToast("저장이 완료되었습니다.", "");
+    } catch {
+      popToast("저장에 실패했습니다.", "다시 시도해 주세요.");
+    }
+  };
+
   return (
     <FormProvider {...method}>
-      <div className={profileStyles.container}>
-        <Preview />
-        <ProfileEdit />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)} className={profileStyles.form}>
+        <div className={profileStyles.container}>
+          <Preview />
+          <ProfileEdit />
+        </div>
+        <SubmitProfile />
+      </form>
     </FormProvider>
   );
 };
