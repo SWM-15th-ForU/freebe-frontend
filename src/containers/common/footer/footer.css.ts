08f8c38cab2c96e9620ab8c839143cd7853495ea
eujin-shin <eujinshin0124@gmail.com>
2024-09-24 14:30:04 +0900

    FU-220 feat: add service footer component

---
+++ src/containers/common/footer/footer.css.ts
@@ -0,0 +1,70 @@
+import { breakpoints } from "@/styles/breakpoints.css";
+import sprinkles from "@/styles/sprinkles.css";
+import { texts } from "@/styles/text.css";
+import { styleVariants } from "@vanilla-extract/css";
+
+export const footerStyles = styleVariants({
+  container: [
+    sprinkles({ backgroundColor: "text-01" }),
+    {
+      marginTop: "auto",
+      width: "100%",
+      padding: "32px 60px 60px 60px",
+      "@media": {
+        [breakpoints.mobile]: {
+          padding: "32px 20px 40px 20px",
+        },
+      },
+    },
+  ],
+  wrapper: {
+    justifyContent: "space-between",
+    display: "flex",
+    "@media": {
+      "screen and (max-width: 967px)": {
+        flexDirection: "column",
+      },
+    },
+  },
+  info: {
+    margin: "20px 0px 20px 0px",
+    display: "flex",
+    flexDirection: "column",
+    gap: 5,
+  },
+  item: {
+    display: "flex",
+    gap: 8,
+  },
+  menuWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    "@media": {
+      "screen and (max-width: 967px)": {
+        marginBottom: 40,
+        marginTop: 20,
+      },
+      [breakpoints.mobile]: {
+        flexDirection: "column",
+        gap: 20,
+      },
+    },
+  },
+  menu: {
+    width: 180,
+    display: "flex",
+    flexDirection: "column",
+    gap: 4,
+  },
+  title: [
+    sprinkles({ color: "white" }),
+    { marginBottom: 8, fontSize: 15, fontWeight: 600 },
+  ],
+  name: [
+    sprinkles({ color: "white" }),
+    { minWidth: "fit-content" },
+    { fontSize: 13, fontWeight: 600 },
+  ],
+  content: [sprinkles({ color: "white" }), { fontSize: 13, fontWeight: 400 }],
+  caption: [texts["caption-01"], sprinkles({ color: "text-04" })],
+});
---
+++ src/containers/common/footer/index.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import { footerStyles } from "./footer.css";
+
+const ServiceFooter = () => {
+  const SERVICE_INFO = [
+    { name: "이메일", content: "swm15foru@gmail.com" },
+    { name: "전화번호", content: "070-8098-6471" },
+    {
+      name: "주소",
+      content: "경기도 김포시 김포한강10로133번길 127, 530-AC03호",
+    },
+    {
+      name: "사업자등록번호",
+      content: "385-13-02383",
+    },
+  ];
+
+  return (
+    <div className={footerStyles.container}>
+      <div className={footerStyles.wrapper}>
+        <div>
+          <Image
+            src="/icons/freebe-logo-white.svg"
+            width={111}
+            height={24}
+            alt="free:be"
+          />
+          <div className={footerStyles.info}>
+            {SERVICE_INFO.map((item) => {
+              return (
+                <div className={footerStyles.item} key={item.name}>
+                  <span className={footerStyles.name}>{item.name}</span>
+                  <span className={footerStyles.content}>{item.content}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={footerStyles.menuWrapper}>
+          <div className={footerStyles.menu}>
+            <span className={footerStyles.title}>이용안내</span>
+            <span className={footerStyles.content}>공지사항</span>
+          </div>
+          <div className={footerStyles.menu}>
+            <span className={footerStyles.title}>서비스 약관</span>
+            <span className={footerStyles.content}>서비스 이용약관</span>
+            <span className={footerStyles.content}>개인정보처리방침</span>
+          </div>
+        </div>
+      </div>
+      <span className={footerStyles.caption}>
+        프리비는 통신판매중개자로서 통신판매의 당사자가 아니며 각 상품의 주문,
+        거래 및 환불 등에 관한 의무와 책임은 각 판매자에게 있습니다.
+      </span>
+    </div>
+  );
+};
+
+export default ServiceFooter;
