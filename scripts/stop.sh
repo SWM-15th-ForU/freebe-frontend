825191d99e41afdb32af575c27fb9463b5f16dff
정유석 <83908712+yuseok0215@users.noreply.github.com>
2024-09-24 15:14:51 +0900

    FU-235 feat: stop.sh 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# PM2 프로세스 이름 (또는 ID)
-APP_NAME="freebe3000"
+# # PM2 프로세스 이름 (또는 ID)
+# APP_NAME="freebe3000"
 
-# 로그 파일 경로
-LOG_FILE="delete.log"
+# # 로그 파일 경로
+# LOG_FILE="delete.log"
 
-# 현재 PM2 프로세스를 찾고 삭제
-if sudo pm2 pid "$APP_NAME"; then
-    echo "Stopping existing PM2 process for $APP_NAME..." | tee -a $LOG_FILE
-    sudo pm2 delete "$APP_NAME" >> $LOG_FILE 2>&1
-else
-    echo "No existing PM2 process found for $APP_NAME." | tee -a $LOG_FILE
-fi
+# # 현재 PM2 프로세스를 찾고 삭제
+# if sudo pm2 pid "$APP_NAME"; then
+#     echo "Stopping existing PM2 process for $APP_NAME..." | tee -a $LOG_FILE
+#     sudo pm2 delete "$APP_NAME" >> $LOG_FILE 2>&1
+# else
+#     echo "No existing PM2 process found for $APP_NAME." | tee -a $LOG_FILE
+# fi
+
+sudo pm2 delete freebe3000
