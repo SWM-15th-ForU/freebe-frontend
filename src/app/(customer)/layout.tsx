f6d3f137a7c47576cfc77f30181219616c40af8c
eujin-shin <eujinshin0124@gmail.com>
2024-09-26 12:01:31 +0900

    FU-225 feat: set metadata

--- src/app/(customer)/layout.tsx
+++ src/app/(customer)/layout.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from "next";
 import ServiceFooter from "@/containers/common/footer";
+import { METADATA } from "@/constants/metadata";
 import { contentStyle, layoutStyle } from "./customer.css";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `${METADATA.customerTitle} | ${METADATA.sitename}`,
+  };
+}
 
 const CustomerLayout = ({
   children,
