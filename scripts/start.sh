7ea499189d5d62d47ebc249ac65a86b6f62817f3
정유석 <83908712+yuseok0215@users.noreply.github.com>
2024-09-26 18:46:43 +0900

    [FU-258] 배포 자동화 스크립트 버그 픽스 (#34)
    * FU-258 feat: start.sh 수정
    
    * FU-258 feat: pr 이벤트 트리거 임시 추가
    
    * Update start.sh
    
    * FU-258 fix: 오탈자 수정
    
    * FU-258 fix: 스크립트 파일 수정
    
    * FU-258 feat: appspec.yml 수정
    
    * FU-258 feat: stop.sh 수정
    
    * FU-258 feat: stop.sh 수정
    
    * FU-258 feat: stop.sh 수정
    
    * FU-258 feat: appspec.yml 수정
    
    * FU-258 feat: appspec.yml 수정
    
    * FU-258 feat: cd_workflow_dev.yml 수정
    
    * FU-258 feat: cd_workflow_dev.yml 수정
    
    * FU-258 feat: stop.sh 수정
    
    * FU-258 feat: stop.sh 수정
    
    * FU-258 feat: stop.sh 수정
    
    * FU-258 feat: stop.sh 수정
    
    * FU-258 feat: stop.sh edit
    
    * FU-258 chore: stop.sh 로그 추가
    
    * FU-258 fix: cd 경로 수정
    
    * FU-258 chore: log 파일 절대경로로 변경
    
    * FU-258 feat: appspec.yml 수정
    
    * FU-258 fix: stop.sh 실행중인 프로세스에 대한 분기처리 수정
    
    * FU-258 chore: stop.sh 로그 추가
    
    * FU-258 chore: 로그 출력문 수정
    
    * FU-258 feat: appspec.yml 수정
    
    * FU-258 feat: appspec.yml 수정
    
    * Update appspec.yml
    
    * FU-258 workflow 수정
    
    * FU-258 chore: start.sh 로그 구체화
    
    ---------
    
    Co-authored-by: eujin-shin <eujinshin0124@gmail.com>
    Co-authored-by: 이유리 <124870949+rheeri@users.noreply.github.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,21 +7,23 @@ set +o allexport
 
 # 경로 및 파일 설정
 ROOT_PATH="/home/ubuntu/freebe-frontend"
-
 LOG_FILE="$ROOT_PATH/start.log"
+NOW=$(date "+%Y %b %d %a %H:%M:%S")
+
+echo "--------------------------------------------------" | tee -a $LOG_FILE
 
 cd $ROOT_PATH
 
+echo "[$NOW] npm install start" | tee -a $LOG_FILE
 npm install
+
+echo "[$NOW] npm run build start" | tee -a $LOG_FILE
+npm run build
 
 # 로그 기록 시작
 {
-    echo "Starting deployment..."
-    echo "Loading environment variables..."
-    
-    # 애플리케이션 실행
-    echo "Starting PM2 process..."
-    sudo pm2 start ecosystem_3000.config.js
+    echo "[$NOW] start pm2 process"
+    pm2 start ecosystem_3000.config.js
 
-    echo "Deployment completed."
+    echo "[$NOW] Deployment completed."
 } >> "$LOG_FILE" 2>&1
