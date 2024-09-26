54d20c43b64d75f36153b35b1e6055227830c899
정유석 <83908712+yuseok0215@users.noreply.github.com>
2024-09-26 11:58:27 +0900

    FU-258 feat: stop.sh 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 # PM2 프로세스 이름 (또는 ID)
 APP_NAME="freebe3000"
-
+NOW=$(date "+%Y %b %d %a %H:%M:%S")
 # 로그 파일 경로
 LOG_FILE="delete.log"
 
@@ -10,9 +10,9 @@ cd /home/ubuntu/freebe-fronted
 
 # 현재 PM2 프로세스를 찾고 삭제
 if pm2 pid "$APP_NAME"; then
-    echo "Stopping existing PM2 process for $APP_NAME..." | tee -a $LOG_FILE
+    echo "[$NOW] Stopping existing PM2 process for $APP_NAME..." | tee -a $LOG_FILE
     pm2 delete "$APP_NAME" >> $LOG_FILE 2>&1
 else
-    echo "No existing PM2 process found for $APP_NAME." | tee -a $LOG_FILE
+    echo "[$NOW] No existing PM2 process found for $APP_NAME." | tee -a $LOG_FILE
 fi
 
