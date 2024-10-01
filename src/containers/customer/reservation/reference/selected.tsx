d774c2c89714e7c9d9b58aef04b81ee70774cf5d
eujin-shin <eujinshin0124@gmail.com>
2024-10-01 19:04:03 +0900

    FU-266 feat: set minimum length at reservation reference image

--- src/containers/customer/reservation/reference/selected.tsx
+++ src/containers/customer/reservation/reference/selected.tsx
@@ -23,7 +23,8 @@ const ReferenceSelected = ({
       }}
     >
       <span className={texts["body-01"]} style={{ color: BLUE03 }}>
-        최대 3장 선택 가능하며, 고른 사진은 작가님께 촬영용 레퍼런스로 전달돼요.
+        내가 원하는 분위기와 비슷한 사진을 최소 1장, 최대 3장 선택할 수 있어요.
+        고른 사진은 작가님께 촬영용 레퍼런스로 전달돼요.
       </span>
       <div className={reservationStyles.imageWrapper}>
         {images
