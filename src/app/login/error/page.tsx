c2c2b4586dc67fbea0faca30a39639d01fde12b2
eujin-shin <eujinshin0124@gmail.com>
2024-10-04 17:28:49 +0900

    FU-262 feat: login error page UI

--- src/app/login/error/page.tsx
+++ src/app/login/error/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { handlerStyles } from "@/app/root.css";
-import { FinishButton } from "@/components/buttons/common-buttons";
+import { CustomButton } from "@/components/buttons/common-buttons";
 import Link from "next/link";
 
 const LoginErrorPage = () => {
   return (
     <div className={handlerStyles.fullContainer}>
-      <span>로그인에 실패했습니다.</span>
+      <span className={handlerStyles.message}>로그인에 실패했습니다.</span>
       <Link href="/login">
-        <FinishButton onClick={() => {}} title="메인으로 돌아가기" />
+        <CustomButton title="메인으로 돌아가기" styleType="primary" size="sm" />
       </Link>
     </div>
   );
