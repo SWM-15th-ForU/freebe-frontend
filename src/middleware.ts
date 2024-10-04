8bf2f42ea26d0641f85bdee46914c9311d40358a
eujin-shin <eujinshin0124@gmail.com>
2024-10-04 17:12:35 +0900

    FU-262 feat: redirect by access token and roletype at middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,56 @@
 import { NextRequest, NextResponse } from "next/server";
+import { tokenKeys } from "./constants/auth";
 
 export async function middleware(request: NextRequest) {
-  return new NextResponse();
+  console.log("middleware run");
+  const url = request.nextUrl;
+  const { pathname } = url;
+
+  if (pathname.startsWith("/login/customer")) {
+    return NextResponse.next();
+  }
+
+  const accessToken = request.cookies.get(tokenKeys.access);
+  const roleType = request.cookies.get(tokenKeys.user)?.value;
+
+  const isLoggedIn = accessToken !== undefined;
+  const isPhotographer = roleType === "photographer";
+
+  if (pathname.startsWith("/login")) {
+    if (isLoggedIn && isPhotographer) {
+      return NextResponse.redirect(new URL("/photographer", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (pathname.startsWith("/photographer")) {
+    if (!isLoggedIn || !isPhotographer) {
+      return NextResponse.redirect(new URL("/login/photographer", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (pathname.startsWith("/customer")) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(
+        new URL(
+          `/login/customer?redirect=${encodeURIComponent(pathname)}`,
+          request.url,
+        ),
+      );
+    }
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/about/:path*",
+  matcher: [
+    "/login",
+    "/login/customer",
+    "/login/photographer",
+    "/photographer/:path*",
+    "/customer/:path*",
+  ],
 };
