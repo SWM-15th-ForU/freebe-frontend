24eb46df1a5003a997f94b8ba9232ec3675448a2
eujin-shin <eujinshin0124@gmail.com>
2024-10-09 17:05:26 +0900

    FU-290 feat: edit information text at profile page

--- src/containers/photographer/mypage/profile/edit/basic-info.tsx
+++ src/containers/photographer/mypage/profile/edit/basic-info.tsx
@@ -73,9 +73,9 @@ const BasicInfo = () => {
         <div className={editStyles.caption}>
           <Image src="/icons/error.svg" alt="확인" height={20} width={12} />
           <span>
-            설정한 연락처는 작가님과 예약을 확정한 고객에게만 전달되며, 이후에도
-            수정 가능합니다. 고객이 연락할 수 있는 수단을 등록해주세요.
-            (전화번호, 오픈채팅방 링크, 인스타그램 DM 등)
+            설정한 연락처는 작가님과 예약을 확정한 고객에게만 전달됩니다. 고객이
+            연락할 수 있는 수단을 등록해주세요. (전화번호, 오픈채팅방 링크,
+            인스타그램 DM 등)
           </span>
         </div>
         {errors.contact && (
