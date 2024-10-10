aacd2b2db81ba931bdb166369d05e73ad67cf490
eujin-shin <eujinshin0124@gmail.com>
2024-10-10 21:33:15 +0900

    FU-280 feat: add temporal api requests

---
+++ src/services/client/photographer/mypage/notice.ts
@@ -0,0 +1,5 @@
+import { NoticeForm } from "profile-types";
+
+export async function putNewNotices(data: NoticeForm) {
+  // TODO: api 명세 확인 후 구현
+}
---
+++ src/services/server/photographer/mypage/notice.ts
@@ -0,0 +1,6 @@
+import { Notice } from "profile-types";
+
+export async function getCurrentNotices(): Promise<Notice[]> {
+  // TODO: api 명세 확인 후 구현
+  return [];
+}
