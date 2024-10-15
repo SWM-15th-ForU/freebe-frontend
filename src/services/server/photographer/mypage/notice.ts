9ce1b66e19685847aea8c6adc70614492fa8981c
eujin-shin <eujinshin0124@gmail.com>
2024-10-15 11:14:42 +0900

    FU-280 fix: fix default value of current notices

--- src/services/server/photographer/mypage/notice.ts
+++ src/services/server/photographer/mypage/notice.ts
@@ -3,7 +3,12 @@ import { api } from "../../core";
 
 export async function getCurrentNotices(): Promise<Notice[]> {
   const { data } = await api
-    .get("photographer/profile")
-    .json<{ data: Notice[] | null }>();
-  return data || [];
+    .get("photographer/notice")
+    .json<{ data: Notice[] }>();
+  return data?.length !== 0
+    ? data
+    : [
+        { title: "취소 및 환불 규정", content: "" },
+        { title: "예약 변경 규정", content: "" },
+      ];
 }
