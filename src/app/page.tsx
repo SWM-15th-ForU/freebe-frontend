e1034df2b3fcb1e8f4d16599b3f0d29bc6d776de
eujin-shin <eujinshin0124@gmail.com>
2024-10-15 20:31:41 +0900

    FU-296 feat: set images for login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
+import { IMAGE_STACK_SOURCES } from "@/constants/common/common";
 import Login from "@/containers/photographer/login";
 import Header from "@/containers/photographer/main/header";
 import { loginPageStyles } from "./login/page.css";
 
 const Page = () => {
-  // TODO: 실제 사진 반환받아 데이터 연결
-  const images = [
-    "https://picsum.photos/800/800",
-    "https://picsum.photos/800/1000",
-    "https://picsum.photos/800/900",
-  ];
-
   return (
     <div className={loginPageStyles.layout}>
       <Header
@@ -18,7 +12,7 @@ const Page = () => {
       />
 
       <div className={loginPageStyles.background}>
-        <Login images={images} />
+        <Login images={IMAGE_STACK_SOURCES} />
       </div>
     </div>
   );
--- src/constants/common/common.ts
+++ src/constants/common/common.ts
@@ -1,1 +1,8 @@
 export const PARAMETER_DEFAULT_RADIX = 10;
+
+export const IMAGE_STACK_SOURCES = [
+  "/images/login/temp-1.jpeg",
+  "/images/login/temp-2.jpeg",
+  "/images/login/temp-3.jpeg",
+  "/images/login/temp-5.jpeg",
+];
