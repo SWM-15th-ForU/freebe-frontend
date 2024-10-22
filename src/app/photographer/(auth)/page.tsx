e2308979ce49da7ebd04c4a5e3de18a6add6ac27
eujin-shin <eujinshin0124@gmail.com>
2024-10-22 16:44:27 +0900

    FU-219 feat: start tutorial when joined

--- src/app/photographer/(auth)/page.tsx
+++ src/app/photographer/(auth)/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import MainView from "@/containers/photographer/main";
+import PhotographerSidebar from "@/containers/photographer/main/sidebar";
+import { photographerStyles } from "./photographer.css";
 
 export default function Home() {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [isOnTutorial, setIsOnTutorial] = useState(false);
 
   useEffect(() => {
     const urlParam = searchParams.get("url");
+    const isTutorial = searchParams.get("tutorial");
+    if (isTutorial) {
+      setIsOnTutorial(true);
+    }
     if (urlParam) {
       const decodedUrl = decodeURIComponent(urlParam);
       localStorage.setItem("url", decodedUrl);
@@ -17,5 +24,13 @@ export default function Home() {
       router.refresh();
     }
   }, [router, searchParams]);
-  return <MainView />;
+
+  return (
+    <div className={photographerStyles.body}>
+      <PhotographerSidebar />
+      <div className={photographerStyles.content}>
+        <MainView tutorialParam={isOnTutorial} />
+      </div>
+    </div>
+  );
 }
