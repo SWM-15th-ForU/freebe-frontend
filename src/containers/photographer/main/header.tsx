9704f81373c4ad5a485e8dcdbe3fe2a4139ecac3
eujin-shin <eujinshin0124@gmail.com>
2024-10-23 10:38:10 +0900

    FU-311 feat: add mobile menu at login pages

--- src/containers/photographer/main/header.tsx
+++ src/containers/photographer/main/header.tsx
@@ -3,11 +3,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import { LinkType } from "profile-types";
-import { Drawer } from "@mantine/core";
+import { Drawer, Menu } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { SERVICE_LINKS } from "@/constants/common/common";
 import CloseButton from "@/components/buttons/close-button";
 import * as styles from "./header/header.css";
 import MenuList from "./sidebar/menu-list";
+import { menuStyles } from "./header/header.css";
 
 const Header = ({
   isOnboarding,
@@ -51,7 +53,45 @@ const Header = ({
           ))}
         </div>
       )}
-      {!isOnboarding && (
+      {isOnboarding ? (
+        <div className={menuStyles.mobileDisplay}>
+          <Menu
+            classNames={{
+              dropdown: menuStyles.dropdown,
+              itemLabel: menuStyles.item,
+            }}
+          >
+            <Menu.Target>
+              <Image
+                src="/icons/down-skyblue.svg"
+                alt="메뉴"
+                width={15}
+                height={10}
+              />
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Link
+                href={SERVICE_LINKS.help}
+                className={menuStyles.item}
+                target="_blank"
+              >
+                <Menu.Item>
+                  <span>고객센터</span>
+                </Menu.Item>
+              </Link>
+              <Link
+                href={SERVICE_LINKS.landingPage}
+                className={menuStyles.item}
+                target="_blank"
+              >
+                <Menu.Item>
+                  <span>서비스 소개</span>
+                </Menu.Item>
+              </Link>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
+      ) : (
         <>
           <button
             type="button"
