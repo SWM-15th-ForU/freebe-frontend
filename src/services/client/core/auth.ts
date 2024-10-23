f49de519071bea12a62c1a5b94d9345e50830432
eujin-shin <eujinshin0124@gmail.com>
2024-10-23 11:13:51 +0900

    FU-312 fix: fix logout response handler

--- src/services/client/core/auth.ts
+++ src/services/client/core/auth.ts
@@ -7,7 +7,6 @@ export const logout = async () => {
       localStorage.removeItem("url");
       return response.url;
     }
-    throw new Error();
   } catch {
     popToast("다시 시도해 주세요.", "로그아웃에 실패했습니다.");
   }
